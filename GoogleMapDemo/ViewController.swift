d4ef91db986bfb9e65b0368b4978c53b88d7fcfc
ngo275 <ngo38114@gmail.com>
2019-05-19 16:31:09 +0700

    Initial Commit

---
+++ GoogleMapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapDemo
+//
+//  Created by ShuichiNagao on 2019/05/19.
+//  Copyright © 2019 Shuichi Nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
